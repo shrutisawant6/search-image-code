76b695dd3b3aa88262d0969d9049c2dec6e59e4b
shrutisawant6 <140047758+shrutisawant6@users.noreply.github.com>
2024-10-15 16:25:11 +0530

    Create Score.cs

---
+++ Constants/Score.cs
@@ -0,0 +1,13 @@
+namespace Sage.Constants
+{
+    public static class Score
+    {
+        public const decimal Value9999 = 0.9999m;
+
+        public const decimal Value9m = 0.9m;
+
+        public const decimal Value8m = 0.8m;
+
+        public const decimal Value45 = 0.45m;
+    }
+}
